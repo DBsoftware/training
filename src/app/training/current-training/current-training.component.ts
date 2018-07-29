9208a46f6036829c21f23c511d46c803f7ead0d8
Diego Bolanos <blueashym@gmail.com>
2018-07-29 18:25:56 -0500

    Primer commit del proyecto

---
+++ src/app/training/current-training/current-training.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-training',
+  templateUrl: './current-training.component.html',
+  styles: []
+})
+export class CurrentTrainingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/training/new-training/new-training.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-training',
+  templateUrl: './new-training.component.html',
+  styles: []
+})
+export class NewTrainingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
